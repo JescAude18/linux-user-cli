7a3f426350ae26189b994388bff81f4de1f439a9
JescAude18 <jessicamoussougan@gmail.com>
2026-04-13 10:07:29 +0100

    feat :sparkles: : improved the script by adding a loop which displays the menu until user type 'exit' ;  added checking of user before deletion

--- user-cli.sh
+++ user-cli.sh
@@ -10,53 +10,68 @@
 echo -e "Welcome to the linux-user-cli project !\n"
 echo -e "Choose one option in the menu below.\n"
 
-echo "MENU"
-echo "1 - Create user"
-echo "2 - Delete user"
-echo "3 - Check user"
-echo -e "4 - Exit\n"
-
-read option
-case "$option" in
-    "1")
-        echo "Enter the name of the user you want to create :"
-        read username
-        echo "Enter its password :"
-        read -s passw1
-        echo "Confirm the password :"
-        read -s passw2
-        if [[ "$passw1" == "$passw2" ]]; then
-            echo "Good password"
-            # useradd ${username}
-            # passwd ${username}
-        else
-            echo "Wrong password"
-        fi
-        ;;
-    "2")
-        echo "Enter the name of the user you want to delete :"
-        read username
-        echo "Do you want to delete the user's directory ? Type yes or no."
-        read y_n
-        if [[ "${y_n,,}" == "yes" ]]; then
-            echo "Bye bye ${username} !"
-            # userdel -r ${username}
-        else
-            echo "type yes or no."
-            # userdel ${username}
-        fi
-        ;;
-    "3")
-        echo "Enter the name of the user you want to check :"
-        read username
-        if [ id ${username} ]; then
-            echo "User ${username} exists."
-        else
-            echo "User ${username} doesn't exist."
-        fi
-        ;;
-    "4")
-        echo "Bye bye ! Mouah !"
-        exit 0
-        ;;
-esac
+while true; do
+    echo "-----------------------------| MENU |------------------------------"
+    echo "|                         1 - Create user                         |" 
+    echo "|                         2 - Delete user                         |"
+    echo "|                         3 - Check user                          |"
+    echo "|                         4 - Exit                                |"
+    echo -e "-------------------------------------------------------------------\n"
+    read -r option
+    case "$option" in
+        "1")
+            echo "Enter the name of the user you want to create :"
+            read username
+            echo "Enter its password :"
+            read -rs passw1
+            echo "Confirm the password :"
+            read -rs passw2
+            if [[ "$passw1" == "$passw2" ]]; then
+                # useradd ${username}
+                # passwd ${username}
+                echo -e "User ${username} created successfully !\n"
+            else
+                echo -e "Wrong password. Retry.\n"
+            fi
+            ;;
+        "2")
+            echo "Enter the name of the user you want to delete :"
+            read username
+            echo "Do you want to delete the user's directory ? Type yes or no."
+            read y_n
+            if [[ "${y_n,,}" == "yes" ]]; then
+                if id "$username">/dev/null 2>&1; then
+                    # userdel -r ${username}
+                    echo -e "Bye bye ${username} !\n"
+                else
+                    echo -e "User ${username} doesn't exist. Type a valid user to delete.\n"
+                fi
+            elif [[ "${y_n,,}" == "no" ]]; then
+                if id "$username">/dev/null 2>&1; then
+                    # userdel ${username}
+                    echo -e "Bye bye ${username} !\n"
+                else
+                    echo -e "User ${username} doesn't exist. Type a valid user to delete.\n"
+                fi
+            else
+                echo -e "Type yes or no.\n"
+            fi
+            ;;
+        "3")
+            echo "Enter the name of the user you want to check :"
+            read username
+            if id "$username">/dev/null 2>&1; then
+                echo -e "User ${username} exists.\n"
+            else
+                echo -e "User ${username} doesn't exist.\n"
+            fi
+            ;;
+        "4")
+            echo -e "Bye bye ! Thanks to coming to the linux-user-cli projet 🥂\n"
+            break
+            ;;
+        *)
+            echo -e "Type a valid value : 1, 2, 3 or 4\n"
+            ;;
+    esac
+done
